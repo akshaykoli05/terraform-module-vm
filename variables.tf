57f2128c2df96a4177437435ea99ddcc29cea4e9
akshaykoli05 <koliakshay05@gmail.com>
2026-03-09 09:28:07 +0530

    removed few variables

--- variables.tf
+++ variables.tf
@@ -1,11 +1,7 @@
-variable "vm_name" {}
 variable "vm_size" {}
 variable "location" {}
 variable "rg_name" {}
 variable "nic_id" {}
-variable "os_type" {
-  description = "Operating system type (Linux/Windows)"
-}
 variable "vm_suffix" {
-  description = "Numeric suffix for VM name (e.g., 01, 02)"
+  description = "Numeric suffix for VM name (e.g., 01)"
 }
